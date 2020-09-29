b3d9f90daba17a886632dc280eb965c758932f49
Kaustuv <karkikaustuv@gmail.com>
2020-09-29 23:15:18 +0545

    landing page somewhat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
-function App() {
+import Jumbotron from './components/jumbotron/index';
+import jumboData from './fixtures/jumbo.json'
+export default function App() {
   return (
-    <div className="app">
-
-    </div>
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt}/>
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom'
 import App from './App';
+import {GlobalStyles} from './global-styles'
+import 'normalize.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+render(
+  <>
+  <GlobalStyles/>
+  <App />
+  </>
+,document.getElementById('root'));
