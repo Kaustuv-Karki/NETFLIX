4d3d07cc8d7ca67586d1da0df29c0bb243136ffd
Kaustuv <karkikaustuv@gmail.com>
2020-10-10 00:26:38 +0545

    signin form outline done no auth

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { HeaderContainer } from '../containers/header'
+import { FooterContainer } from '../containers/footer'
+import { Form } from '../components'
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase'
+import * as ROUTES from '../constants/routes'
 
 export default function SignIn() {
+
+    const { firebase } = useContext(FirebaseContext);
+    const [ error, setError ] = useState('')
+    const [ emailAddress, setEmailAddress ] = useState('')
+    const [ password, setPassword ] = useState('')
+
+    const isInvalid = password === '' || emailAddress === '';
+
+    const handleSignIn = (event) => {
+        event.preventDefault()
+    }
+
     return (
-        <div>
-            
-        </div>
+        <>
+        <HeaderContainer>Hello From SignIn
+            <Form>
+                <Form.Title>Sign In</Form.Title>
+                {error && <Form.Error>{error}</Form.Error>}
+                <Form.Base onSubmit={handleSignIn} method="POST" >
+                    <Form.Input  
+                        placeholder="Email Address"
+                        value={emailAddress}
+                        onChange= {e => setEmailAddress(e.target.value)}
+                        />
+                    <Form.Input 
+                        placeholder="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        />
+                    <Form.Submit  disabled={isInvalid} type="submit" data-testid="sign-in">
+                        Sign In
+                    </Form.Submit>
+                </Form.Base>
+                    <Form.Text>
+                        New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+                    </Form.Text>
+                    <Form.TextSmall>
+                        This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+                    </Form.TextSmall>
+            </Form>
+        </HeaderContainer>
+        <FooterContainer/>
+        </>
     )
 }
